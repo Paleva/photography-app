e181e491b79c32f033c7e8aec25e07938f8aebb4
Paleva <arnas.voitkevicius@gmail.com>
2025-04-16 12:01:25 +0300

    Hosting files to self host the app

---
+++ hosting/schema.sql
@@ -0,0 +1,80 @@
+-- USERS table: Stores user accounts (Photographers, Viewers, Admins)
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,  -- Hashed password
+    role VARCHAR(50),
+    profile_picture TEXT DEFAULT '/default-profile-picture.png' NOT NULL,  -- URL to profile image
+    bio TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CATEGORIES table: Stores different photo categories
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL
+);
+-- PHOTOS table: Stores uploaded photos
+CREATE TABLE posts (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    filename VARCHAR(255) NOT NULL,
+    description TEXT,
+    user_id INT REFERENCES users(id),
+    file_path TEXT NOT NULL,  -- API file location
+    real_path TEXT NOT NULL, -- Real file location on the system
+    category_id INT REFERENCES categories(id) ON DELETE SET NULL,
+    likes INT DEFAULT 0, 
+    isvertical BOOLEAN,
+    uploaded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- COMMENTS table: Stores user comments on photos
+CREATE TABLE comments (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id) ON DELETE CASCADE,
+    post_id INT REFERENCES posts(id) ON DELETE CASCADE,
+    comment_text TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE likes(
+    post_id INT REFERENCES posts(id) ON DELETE CASCADE,
+    user_id INT REFERENCES users(id) ON DELETE CASCADE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY( post_id, user_id)
+);
+
+CREATE TABLE roles(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+
+-- TRIGGER FOR posts.likes SUM TO CHANGE EVERY INSERT OR DELETE ON LIKES TABLE
+CREATE OR REPLACE FUNCTION update_likes_count()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF TG_OP = 'INSERT' THEN
+        UPDATE posts
+        SET likes = likes + 1
+        WHERE id = NEW.post_id;
+    ELSIF TG_OP = 'DELETE' THEN
+        UPDATE posts
+        SET likes = likes - 1
+        WHERE id = OLD.post_id;
+    END IF;
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_increment_likes
+AFTER INSERT ON likes
+FOR EACH ROW
+EXECUTE FUNCTION update_likes_count();
+
+CREATE TRIGGER trigger_decrement_likes
+AFTER DELETE ON likes
+FOR EACH ROW
+EXECUTE FUNCTION update_likes_count();
+
